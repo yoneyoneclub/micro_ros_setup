8ed4eac2ce7dc497b2cf7bb779398dbae871c210
yoneyoneclub <takumi.yonemoto@gekidaniino.co.jp>
2024-06-13 21:33:40 +0900

    fix:folder path

--- scripts/create_agent_ws.sh
+++ scripts/create_agent_ws.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 
 PREFIX=$(ros2 pkg prefix micro_ros_setup)
-TARGETDIR=src
+TARGETDIR=src/iino/external/micro_ros
 
 if [ -z ${EXTERNAL_SKIP+x} ]; then
   EXTERNAL_SKIP=""
